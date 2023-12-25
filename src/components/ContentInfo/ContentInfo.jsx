97a783c926368adc9339aee1386c33da6b4b861b
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2023-12-25 13:43:50 +0100

    added contentInfo card styles

--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -13,7 +13,24 @@ const ContentInfo = () => {
 
   return (
     <div className="contentInfo" data-testid="contentInfo">
-      {contentInfo?.Plot}
+      <h3 className="contentInfo__heading">{contentInfo.Title}</h3>
+      <h4 className="contentInfo__year">Release year: {contentInfo.Year}</h4>
+      <span className="contentInfo__country">
+        Country: {contentInfo.Country}
+      </span>
+      <span className="contentInfo__releaseDate">
+        Released: {contentInfo.Released}
+      </span>
+      <img
+        src={
+          contentInfo.Poster === 'N/A'
+            ? 'https://placehold.co/277x400?text=N/A'
+            : contentInfo.Poster
+        }
+        alt="poster"
+        className="contentInfo__image"
+      />
+      <p className="contentInfo__plot">{contentInfo.Plot}</p>
     </div>
   );
 };
