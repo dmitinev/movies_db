88c11a42a947cb584a455062247e8db51171dba0
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2023-12-27 21:35:36 +0100

    route path / will navigate to main page correctly

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Page404 } from '../pages/404/404.jsx';
 import ContentPage from '../pages/ContentPage/ContentPage.jsx';
 import MainPage from '../pages/MainPage/MainPage.jsx';
 
 const AppRoutes = () => (
   <Routes>
+    <Route path="/" element={<Navigate to="/movies_db/" />} />
     <Route path="/movies_db/" element={<MainPage />} />
     <Route path="/card/:contentId" element={<ContentPage />} />
     <Route path="*" element={<Page404 />} />
